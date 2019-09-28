87f3f37782a3eac50f94e0e7aacdd46801460845
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-28 18:05:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,10 @@ class Triangle
     if side_a <= 0 || side_b <= 0 || side_c <= 0 
       raise TriangleError
     elsif
-      side_a + side_b <= side_c
+    side_a + side_b <= side_c || side_b + side_c <= side_a || side_c + side_a <= side_b
+    raise TriangleError
+    elsif
+    
     
   end 
   
