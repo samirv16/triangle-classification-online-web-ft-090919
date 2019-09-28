776e469c663c607a368d1c11d9e42721bcff6d4a
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-28 17:40:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,13 @@
 class Triangle
   
+  attr_accessor :side_a, :side_b, :side_c
+  
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+  
   
   
 end
