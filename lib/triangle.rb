f933114944a4d26e410d6ea7d5970b42265293ac
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-28 18:28:28 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
   # write code here
+  def initialize(sidea, sideb, sidec)
+    @sidea = sidea
+    @sideb = sideb
+    @sidec = sidec
+  end
+
+  class TriangleError < StandardError
+
+
+  end
+  def kind
+    if (@sidea <= 0) || (@sideb <= 0) || (@sidec <= 0)
+      raise TriangleError
+    elsif (@sidea + @sideb <= @sidec) || (@sidea + @sidec <= @sideb) || (@sideb + @sidec <= @sidea)
+          raise TriangleError
+        else
+    if (@sidea == @sideb) && (@sideb == @sidec)
+      :equilateral
+    elsif (@sidea == @sideb) || (@sideb == @sidec) || (@sidea == @sidec)
+      :isosceles
+    elsif (@sidea != @sideb) && (@sideb != @sidec) && (@sidea != @sidec)
+      :scalene
+    end
+  end
+  end
 end
