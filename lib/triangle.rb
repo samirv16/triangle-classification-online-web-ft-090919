ed0ec70145186e28f69beddf7a8aaed6ba688d7c
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-28 18:24:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,11 +18,11 @@ class Triangle
     (side_a + side_b <= side_c) || (side_b + side_c <= side_a) || (side_c + side_a <= side_b)
       raise TriangleError
     else
-      if side_a && side_b == side_c
+      if (side_a && side_b == side_c)
       :equilateral
-      if side_a == side_b || side_b == side_c || side_c == side_a
+      if (side_a == side_b) || (side_b == side_c) || (side_c == side_a)
       :isosceles
-      if side_a != side_b && side_b != side_c && side_c != side_a
+      if (side_a != side_b) && (side_b != side_c) && (side_c != side_a)
       :scalene
     end 
     end
